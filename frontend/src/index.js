80cc962082a0f4cb46969b2de2e779af8d8ffab0
Olya <116801530+marshennikovaolga@users.noreply.github.com>
2023-12-21 18:29:40 +0300

    deleted dotenv

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,9 +4,6 @@ import './index.css';
 import App from './components/App.jsx';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
